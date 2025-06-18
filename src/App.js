7c16b5d35feb8c6a8d127f4ba36d9827e3f6f30c
ab210496 <118595611+ab210496@users.noreply.github.com>
2025-06-18 13:25:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,26 +28,6 @@ const CurrencyProvider = ({ children }) => {
 };
 const useCurrency = () => useContext(CurrencyContext);
 
-// --- FIREBASE CONFIG ---
-
-import { initializeApp } from "firebase/app";
-import { getAuth, signInAnonymously, signInWithCustomToken, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore, collection, doc, onSnapshot, addDoc, updateDoc, deleteDoc, setDoc, getDoc, setLogLevel } from 'firebase/firestore';
-import { getStorage } from "firebase/storage";
-
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
 
 // --- Custom Logo ---
 const QuantfolioLogo = () => (
